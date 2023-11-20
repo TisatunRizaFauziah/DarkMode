e3c67fb9187e1728bb3653eefe071fda0570ecd7
user <user@DESKTOP-EIT7NK4>
2023-11-20 21:53:12 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace DarkMode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.guna2ToggleSwitch1 = new Guna.UI2.WinForms.Guna2ToggleSwitch();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.indonesia = new System.Windows.Forms.PictureBox();
+            this.inggris = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.indonesia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inggris)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2ToggleSwitch1
+            // 
+            this.guna2ToggleSwitch1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ToggleSwitch1.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ToggleSwitch1.CheckedState.InnerBorderColor = System.Drawing.Color.White;
+            this.guna2ToggleSwitch1.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.guna2ToggleSwitch1.Location = new System.Drawing.Point(158, 107);
+            this.guna2ToggleSwitch1.Name = "guna2ToggleSwitch1";
+            this.guna2ToggleSwitch1.Size = new System.Drawing.Size(53, 30);
+            this.guna2ToggleSwitch1.TabIndex = 2;
+            this.guna2ToggleSwitch1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2ToggleSwitch1.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2ToggleSwitch1.UncheckedState.InnerBorderColor = System.Drawing.Color.White;
+            this.guna2ToggleSwitch1.UncheckedState.InnerColor = System.Drawing.Color.White;
+            this.guna2ToggleSwitch1.CheckedChanged += new System.EventHandler(this.guna2ToggleSwitch1_CheckedChanged);
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 30;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(941, 81);
+            this.guna2Panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(34, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dark Mode";
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this.guna2Panel1;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // indonesia
+            // 
+            this.indonesia.Image = global::DarkMode.Properties.Resources.delete_FILL0_wght400_GRAD0_opsz24;
+            this.indonesia.Location = new System.Drawing.Point(819, 87);
+            this.indonesia.Name = "indonesia";
+            this.indonesia.Size = new System.Drawing.Size(68, 50);
+            this.indonesia.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.indonesia.TabIndex = 5;
+            this.indonesia.TabStop = false;
+            this.indonesia.Click += new System.EventHandler(this.indonesia_Click);
+            // 
+            // inggris
+            // 
+            this.inggris.Image = global::DarkMode.Properties.Resources.save_FILL0_wght400_GRAD0_opsz24;
+            this.inggris.Location = new System.Drawing.Point(818, 87);
+            this.inggris.Name = "inggris";
+            this.inggris.Size = new System.Drawing.Size(69, 50);
+            this.inggris.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.inggris.TabIndex = 0;
+            this.inggris.TabStop = false;
+            this.inggris.Click += new System.EventHandler(this.inggris_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(254, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(463, 40);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Welcome To My Application";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 583);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.indonesia);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.guna2ToggleSwitch1);
+            this.Controls.Add(this.inggris);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.indonesia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inggris)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox inggris;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Guna.UI2.WinForms.Guna2ToggleSwitch guna2ToggleSwitch1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox indonesia;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DarkMode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            indonesia.Visible = false;
+            inggris.Visible = true;
+        }
+
+        private void guna2ToggleSwitch1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(guna2ToggleSwitch1.Checked==true)
+            {
+                this.BackColor = Color.FromArgb(34, 36, 49);
+                label2.ForeColor = Color.White;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+                label2.ForeColor = Color.Black;
+            }
+        }
+
+        private void indonesia_Click(object sender, EventArgs e)
+        {
+            if(indonesia.Visible==true)
+            {
+               indonesia.Visible = false;
+                label2.Text = "Selamat DAtang di Aplikasi saya ";
+
+            }
+        }
+
+        private void inggris_Click(object sender, EventArgs e)
+        {
+            if (inggris.Visible == false)
+            {
+                inggris.Visible = true;
+                label2.Text = "Wolcome to my application ";
+
+            }
+        }
+    }
+}
